5ada7b57f1c9751a8565e6c51ef980ceb7d5c853
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-21 00:53:53 +1200

    Finish brute force solution

---
+++ fibreBrute.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def comboMofo(arr):
+    length = len(arr)
+    count = 1
+    while count <= length:
+        for i in range(length + 1 - count):
+            yield sum(arr[i:i+count])
+        count += 1
+
+
+def calculateProfit(arr):
+    return max(x for x in comboMofo(arr))
+
+
+line = sys.stdin.readline().rstrip().split()
+result = []
+
+while len(line) != 1:
+    result.append(calculateProfit([int(x) for x in line]))
+    line = sys.stdin.readline().rstrip().split()
+
+print("\n".join(str(x) for x in result))
