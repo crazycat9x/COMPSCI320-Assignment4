47f2cc68e7837a45625cc1070c7fabb3a5c5e249
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-21 20:36:47 +1200

    Reimplement brute force

--- fibreBrute.py
+++ fibreBrute.py
@@ -1,24 +1,31 @@
 import sys
 
 
-def comboMofo(arr):
-    length = len(arr)
-    count = 1
-    while count <= length:
-        for i in range(length + 1 - count):
-            yield sum(arr[i:i+count])
-        count += 1
+# def comboGenerator(arr):
+#     length = len(arr)
+#     count = 1
+#     while count <= length:
+#         for i in range(length + 1 - count):
+#             yield sum(arr[i:i+count])
+#         count += 1
 
+# def calculateProfit(arr):
+#     return max(x for x in comboGenerator(arr))
 
-def calculateProfit(arr):
-    return max(x for x in comboMofo(arr))
-
+def fibreBrute(arr):
+    memory = []
+    memory.append([arr.pop(0)])
+    for i in arr:
+        temp = [i+x for x in memory[-1]]
+        temp.append(i)
+        memory.append(temp)
+    return max([max(x) for x in memory])
 
 line = sys.stdin.readline().rstrip().split()
 result = []
 
 while len(line) != 1:
-    result.append(calculateProfit([int(x) for x in line]))
+    result.append(fibreBrute([int(x) for x in line]))
     line = sys.stdin.readline().rstrip().split()
 
 sys.stdout.write("\n".join(str(x) for x in result))
