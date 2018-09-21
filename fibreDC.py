280ea03bf70314b65dbbb10e512b0b49d64e3447
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-21 20:37:40 +1200

    Reimplement divide and conquer

--- fibreDC.py
+++ fibreDC.py
@@ -2,40 +2,38 @@ import sys
 import math
 
 
-# fromSide 0 => left, 1 => right, 2 => side doesn't matter
-def searchPathFromSide(arr, fromSide):
-    length = len(arr)
-    if length == 1:
-        return arr[0]
-    elif length == 2:
-        if fromSide == 1:
-            return max(arr[0], arr[0]+arr[1])
-        elif fromSide == 0:
-            return max(arr[1], arr[0]+arr[1])
-        elif fromSide == 2:
-            return max(arr[0], arr[1], arr[0]+arr[1])
+def crossingFibreDC(arr, low, mid, high):
+    total = 0
+    leftTotal = -math.inf
+    rightTotal = -math.inf
+    for i in range(mid, low-1, -1):
+        total += arr[i]
+        if total > leftTotal:
+            leftTotal = total
+
+    total = 0
+    for i in range(mid+1, high+1):
+        total += arr[i]
+        if total > rightTotal:
+            rightTotal = total
+
+    return leftTotal + rightTotal
+
+
+def fibreDC(arr, low, high):
+    if low == high:
+        return arr[low]
     else:
-        divider = int(math.floor(length/2))
-        left = arr[:divider]
-        right = arr[divider+1:]
-        leftMax = searchPathFromSide(left, 0)
-        rightMax = searchPathFromSide(right, 1)
-        midValue = arr[divider]
-        if fromSide == 1:
-            leftSum = sum(left)
-            return max(leftSum, leftSum+midValue, leftSum+midValue+rightMax)
-        elif fromSide == 0:
-            rightSum = sum(right)
-            return max(rightSum, midValue+rightSum, leftMax+midValue+rightSum)
-        elif fromSide == 2:
-            return max(leftMax, leftMax+midValue, rightMax, midValue+rightMax, leftMax+midValue+rightMax)
+        mid = (low+high) // 2
+        return max(fibreDC(arr, low, mid), fibreDC(arr, mid+1, high), crossingFibreDC(arr, low, mid, high))
 
 
 line = sys.stdin.readline().rstrip().split()
 result = []
 
 while len(line) != 1:
-    result.append(searchPathFromSide([int(x) for x in line], 2))
+    inputArr = [int(x) for x in line]
+    result.append(fibreDC(inputArr, 0, len(inputArr)-1))
     line = sys.stdin.readline().rstrip().split()
 
 sys.stdout.write("\n".join(str(x) for x in result))
